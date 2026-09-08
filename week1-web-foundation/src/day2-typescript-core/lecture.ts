7425b44cfeb73573968ab2073a73e5e939ef2ee8
felix chen <felixchen3674@gmail.com>
2026-09-08 14:03:04 -0400

    remove lab 2 for now

--- week1-web-foundation/src/day2-typescript-core/lecture.ts
+++ week1-web-foundation/src/day2-typescript-core/lecture.ts
@@ -116,23 +116,6 @@ const todo = {
   completed: false,
 };
 
-const user = {
-  id: 1,
-  name: "Leanne Graham",
-  username: "Bret",
-  email: "Sincere@april.biz",
-  address: {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    geo: {
-      lat: "-37.3159",
-      lng: "81.1496",
-    },
-  },
-};
-
 // const data = [
 //   1,
 //   "412",
